9b7056d53ae3333fde9e0796a8c5b8aabe52a185
Pedro Martins <spjm.pt@hotmail.com>
2017-01-15 20:58:44 +0000

    Adds interface to visually warn for next ticket

---
+++ QueuServer/Interfaces/IWindowUpdate.cs
@@ -0,0 +1,14 @@
+﻿using QueueServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuServer.Interfaces
+{
+    interface IWindowUpdate
+    {
+        void TicketsUpdated(ServerUpdate su);
+    }
+}
